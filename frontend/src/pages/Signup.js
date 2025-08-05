9978c8532e3afb7bcac59fcfd482a06942c7df96
Yatharth Verma <yatharthv.noida@gmail.com>
2025-08-05 22:45:57 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `https://mern-auth-px6yghxrp-vermayatharths-projects.vercel.app//auth/signup`;
+            const url = `https://mern-auth-px6yghxrp-vermayatharths-projects.vercel.app/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
