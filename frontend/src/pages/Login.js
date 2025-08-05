61c305a02126242a0a93dd7cbda2d5420fb32f7d
Yatharth Verma <yatharthv.noida@gmail.com>
2025-08-05 23:22:36 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `https://mern-auth-ui-livid.vercel.app/auth/login`;
+            const url = `https://mern-auth-api-taupe.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
