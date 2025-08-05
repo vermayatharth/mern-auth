44651af8da71e8b0096767d34697e7884bea4e1f
Yatharth Verma <yatharthv.noida@gmail.com>
2025-08-05 22:56:20 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = "https://mern-auth-px6yghxrp-vermayatharths-projects.vercel.app/auth/products";
+            const url = "https://mern-auth-ui-livid.vercel.app/auth/products";
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
